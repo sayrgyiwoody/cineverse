bd2d4dc67022622a84da763434fd1f3df96518c1
Woody <waiyanwoody@gmail.com>
2024-04-10 17:32:46 +0630

    mb update

--- src/components/FilterDiv.jsx
+++ src/components/FilterDiv.jsx
@@ -98,7 +98,7 @@ const FilterDiv = () => {
         <div className={`${showSorting ? '' : 'hidden'}`}>
           <div className=" border rounded-b-xl border-gray-200 dark:border-gray-700 dark:bg-gray-800">
             <div className="border-b border-b-gray-300 dark:border-b-gray-700 p-5">
-              <p className="mb-4 text-gray-900 font-semibold dark:text-white">Sort Results By</p>
+              <p className="mb-4 text-gray-900 font-semibold text-lg dark:text-white">Sort Results By</p>
               <select
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary dark:focus:border-primary"
                 value={selectedSorting}
@@ -134,7 +134,7 @@ const FilterDiv = () => {
           <div className=" border rounded-b-xl border-gray-200 dark:border-gray-700 dark:bg-gray-800">
 
             <div className="border-b border-b-gray-300 dark:border-b-gray-700 px-4 py-2 md:p-5">
-              <p className="mb-4 text-lg text-gray-900 font-semibold dark:text-white">Release Dates</p>
+              <p className=" mb-2 md:mb-4 text-lg text-gray-900 font-semibold dark:text-white">Release Dates</p>
 
               <div className="grid grid-cols-5 items-center gap-x-4 mb-4">
                 <label htmlFor="" className=' ms-1 text-gray-600 dark:text-slate-200'>From</label>
@@ -151,7 +151,7 @@ const FilterDiv = () => {
             </div>
 
             <div className="border-b border-b-gray-300 dark:border-b-gray-700 px-4 py-2 md:p-5">
-              <p className="mb-4 text-lg text-gray-900 font-semibold dark:text-white">Genres</p>
+              <p className="mb-3 md:mb-4 text-lg text-gray-900 font-semibold dark:text-white">Genres</p>
 
               <div className="flex flex-wrap ">
                 {genres.map((genre, index) => (
