74b69c4ae8e68cb3ce1f5a9bd97ee40fc4e445f3
Woody <waiyanwoody@gmail.com>
2024-04-11 10:56:01 +0630

    update 404 page

--- src/view/NotFound.jsx
+++ src/view/NotFound.jsx
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom'
 const NotFound = () => {
     return (
         <div>
-            <div className="flex min-h-screen flex-col justify-center items-center mx-auto bg-slate-100 dark:bg-gray-900">
-                <img src='public/images/notFound.png' width="400" alt="notFound" />
-                <p className="text-center dark:text-slate-100">
-                    The page you are trying to access doesn't exist or has been moved.<br className="d-none d-md-block" />
+            <div className="flex min-h-screen flex-col items-center mx-auto bg-slate-100 dark:bg-gray-900">
+                <img src='public/images/notFound.png' className=' max-w-xs' alt="notFound" />
+                <p className="text-center dark:text-slate-100 px-5 md:max-w-max text-xs md:text-base">
+                    The page you are trying to access doesn't exist or has been moved.<br className="hidden md:block" />
                     Try going back to our homepage.
                 </p>
-                <Link to={'/'} className="bg-blue-600 text-white rounded mt-5 hover:bg-blue-600_hover duration-150 px-3 py-2">Go to homepage</Link>
+                <Link to={'/'} className="bg-primary text-white rounded mt-5 hover:bg-primaryHover duration-150 px-4 py-2">Go to homepage</Link>
 
             </div>
 
