3b5fc7d9a4cbf6c72de46e77a29f3951deb7ed3b
Woody <waiyanwoody@gmail.com>
2024-04-11 10:57:01 +0630

    update 404 img url

--- src/view/NotFound.jsx
+++ src/view/NotFound.jsx
@@ -5,7 +5,7 @@ const NotFound = () => {
     return (
         <div>
             <div className="flex min-h-screen flex-col items-center mx-auto bg-slate-100 dark:bg-gray-900">
-                <img src='public/images/notFound.png' className=' max-w-xs' alt="notFound" />
+                <img src='/images/notFound.png' className=' max-w-xs' alt="notFound" />
                 <p className="text-center dark:text-slate-100 px-5 md:max-w-max text-xs md:text-base">
                     The page you are trying to access doesn't exist or has been moved.<br className="hidden md:block" />
                     Try going back to our homepage.
